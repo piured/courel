cd34d92eb091a51c235817a72380d019b28c8843
piulin <pepo_gonba@hotmail.com>
2023-07-16 16:39:15 +0200

    add docs to Isubscriber (#1)

--- Subscription/ISubscriber.cs
+++ Subscription/ISubscriber.cs
@@ -44,20 +44,54 @@ namespace Courel.Subscription
         public void OnHoveringReceptorSingleNotes(Row row);
 
         /// <summary>
-        /// This method is called when a <see cref="Courel.Loader.Notes.Hold"/> is active and in action range.
+        /// This method is called at every update when a <see cref="Courel.Loader.Notes.Hold"/> is active and in action range.
         /// </summary>
-        /// <param name="note"></param>
-        /// <param name="held"></param>
+        /// <param name="note">Target active hold note and in action range.</param>
+        /// <param name="held">True if the hold is held, false otherwise.</param>
         public void OnActiveHold(Hold note, bool held);
 
+        /// <summary>
+        /// This method is called when a <see cref="Courel.Loader.Notes.Hold"/> is active and has reached its end.
+        /// </summary>
+        /// <param name="note">Targe active hold.</param>
+        /// <param name="held">True if the hold is held at the end of its life, false otherwise.</param>
         public void OnHoldEnded(Hold note, bool held);
 
+        /// <summary>
+        /// This method is called when a <see cref="Courel.Loader.Notes.SingleNote"/> is judged due to an input event.
+        /// </summary>
+        /// <param name="note">Judged note.</param>
+        /// <param name="row">Row containing the note that has been judged.</param>
         public void OnJudgedSingleNoteOnRow(SingleNote note, Row row);
 
+        /// <summary>
+        /// This method is called when a <see cref="Courel.Loader.Notes.Hold"/> becomes inactive.
+        /// </summary>
+        /// <param name="note"> Target inactive hold.</param>
         public void OnHoldInactive(Hold note);
+
+        /// <summary>
+        /// This method is called when an active <see cref="Courel.Loader.Notes.Hold"/> is judged at the end of its life.
+        /// </summary>
+        /// <param name="note">Judged active hold.</param>
         public void OnHoldEndJudged(Hold note);
+
+        /// <summary>
+        /// This method is called when a row of <see cref="Courel.Loader.Notes.SingleNote"/> is rolled back.
+        /// </summary>
+        /// <param name="row">Rolled back row.</param>
         public void OnRolledBackSingleNoteRow(Row row);
+
+        /// <summary>
+        /// This method is called when a <see cref="Courel.Loader.Notes.Hold"/> is partially rolled back, becoming active and in action range.
+        /// </summary>
+        /// <param name="note">Partially rolled back hold.</param>
         public void OnHoldIsPartiallyRolledBack(Hold note);
+
+        /// <summary>
+        /// This method is called when a <see cref="Courel.Loader.Notes.Hold"/> is rolled back completely, becoming active and not in action range.
+        /// </summary>
+        /// <param name="note"></param>
         public void OnHoldIsRolledBack(Hold note);
     }
 }
