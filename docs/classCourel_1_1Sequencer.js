c93e128d176b18b4e4f4051ede23b40453af3d69
piulin <pepo_gonba@hotmail.com>
2023-07-18 19:04:35 +0200

    update doxygen (#5)

--- docs/classCourel_1_1Sequencer.js
+++ docs/classCourel_1_1Sequencer.js
@@ -2,6 +2,7 @@ var classCourel_1_1Sequencer =
 [
     [ "AddSubscriber", "classCourel_1_1Sequencer.html#a6c65c431414e209de67047f716d42e99", null ],
     [ "GetDrawableNotes", "classCourel_1_1Sequencer.html#a2f5bdf20f6d808f262cb898f9411abd3", null ],
+    [ "GetLag", "classCourel_1_1Sequencer.html#ac3d56c23a849108797c8b5148e3ef586", null ],
     [ "GetPercentageOfBeat", "classCourel_1_1Sequencer.html#af0185a206015ea2907163f217bf0fc0a", null ],
     [ "GetScroll", "classCourel_1_1Sequencer.html#af6892c77a23afa281a8ae0e6ca3e10c5", null ],
     [ "GetSpeed", "classCourel_1_1Sequencer.html#ac0d2e3b35c34ca602f8e67d10af46545", null ],
@@ -9,11 +10,14 @@ var classCourel_1_1Sequencer =
     [ "GetTotalNumberOfRows", "classCourel_1_1Sequencer.html#a582bf41627202b13d88a5081223440c4", null ],
     [ "GetTotalNumberOfSingleNotes", "classCourel_1_1Sequencer.html#a57e3856c3070b08331ed74a0580d5dad", null ],
     [ "GetTotalNumberOfSingleNotesAccountingForCombo", "classCourel_1_1Sequencer.html#a52278896781d325c3adc688e645a0402", null ],
+    [ "GetUserSpeed", "classCourel_1_1Sequencer.html#acdaaa6ab74f554922a2b3486f8c075d3", null ],
     [ "Lift", "classCourel_1_1Sequencer.html#a3bf30d0e4d2eb20b8d2b3fe3e600f01c", null ],
     [ "LoadChart", "classCourel_1_1Sequencer.html#ac53592777ed26f2aac4fc0ccc358333b", null ],
     [ "SetAutoPlay", "classCourel_1_1Sequencer.html#a4def95ba6efd5e5b0da8b30da3a4f152", null ],
     [ "SetIHoldInput", "classCourel_1_1Sequencer.html#acf24b5b3277afc7444dd086c6b2f8f87", null ],
     [ "SetIJudge", "classCourel_1_1Sequencer.html#aec279da20cd09b3ea2ced76b48e3784c", null ],
     [ "SetISong", "classCourel_1_1Sequencer.html#a14c034cb7ed03b22eb3a8420abd39582", null ],
+    [ "SetLag", "classCourel_1_1Sequencer.html#ac991dd7b9b8c19725ef0c7d33d566516", null ],
+    [ "SetUserSpeed", "classCourel_1_1Sequencer.html#a39907c73987089bc04fa1c7a007d0903", null ],
     [ "Tap", "classCourel_1_1Sequencer.html#a04d4d97d084f284f97058fb09265ea94", null ]
 ];
